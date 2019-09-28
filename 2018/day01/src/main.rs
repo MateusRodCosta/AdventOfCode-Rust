3762fecf952876501b2eae033704501e651cd492
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2019-09-28 11:32:37 -0300

    Solve Advent Of Code 2018 Day 1 Part 2
    Thanks to aladdy on #rust:matrix.org for help with reasoning about Rust

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -3,15 +3,45 @@ use std::fs;
 fn main() {
     let data = fs::read_to_string("input.txt").expect("Unable to read file");
 
-    first_part(data);
+    let iter = data.split_whitespace();
+
+    let mut numbers: Vec<i32> = Vec::new();
+
+    iter.for_each(|f| {
+        let num = f.parse::<i32>().unwrap();
+        numbers.push(num);
+    });
+
+    first_part(&numbers);
+    second_part(&numbers);
 }
 
-fn first_part(data: String) {
-    let iter = data.split_whitespace();
+fn first_part(numbers: &Vec<i32>) {
     let mut value = 0;
-    iter.for_each(|f| {
-        let val = f.parse::<i32>().unwrap();
-        value += val;
-    });
+
+    for num in numbers {
+        value += num;
+    }
+    println!("Result: {}", value);
+}
+
+fn second_part(numbers: &Vec<i32>) {
+    let mut value: i32 = 0;
+
+    let mut vec: Vec<i32> = Vec::new();
+    vec.push(value);
+
+    let mut found = false;
+
+    while !found {
+        for num in numbers {
+            value += num;
+            if vec.contains(&value) {
+                found = true;
+                break;
+            };
+            vec.push(value);
+        }
+    }
     println!("Result: {}", value);
 }
